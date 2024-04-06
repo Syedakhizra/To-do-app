238a380ba5fc2a531d41d61a3735349b4e8df686
Syedakhizra <159184686+Syedakhizra@users.noreply.github.com>
2024-04-06 22:15:40 +0500

    created to do app

---
+++ main.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = "true";
+while (condition) {
+    let todosQuestion = await inquirer.prompt([
+        {
+            name: "questionOne",
+            type: "input",
+            message: "What would you like to add in your todos?"
+        },
+        {
+            name: "questionTwo",
+            type: "confirm",
+            message: "Would you like to add more in your todos?",
+            default: "true"
+        }
+    ]);
+    todos.push(todosQuestion.questionOne);
+    console.log(todos);
+    condition = todosQuestion.questionTwo;
+}
